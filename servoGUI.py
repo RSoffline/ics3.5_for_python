0329ada7e3e7ffa7d35f70b5763eb874d3d80e88
RSoffline <fudansi.ryusei@gmail.com>
2016-02-04 23:21:02 +0900

    add widget

--- servoGUI.py
+++ servoGUI.py
@@ -2,23 +2,70 @@ import serialServo
 import sys
 import Tkinter as tk
 
-serialServo.setSerial()
-
 def scaleFunc(event):
-	value = scale1.get()
-	deg = round(270.0 * (value - 3500.0) / 8000.0, 4)
-	label.configure(text="deg:"+str(deg))
-	servo(deg)
+	deg = scale1.get()
+	posBuff.set(str(deg))
+	servo.Pos(deg)
+	
+def setPos(event):
+	if posBuff.get():
+		value = float(posBuff.get())
+		scale1.set(value)
+		servo(value)
+	
+	
+def selectAdd(event):
+	global add
+	if buffer.get():
+		add = int(buffer.get())
+		servo.addr = add
+		addLabel.configure(text = "address:"+str(add))
 
-servo = serialServo.Servo(0)
+def setPort(event):
+	value = portBuff.get()
+	serialServo.setSerial(value)
+	
+
+def servo(x):
+	print add, int(3500 + 8000 * x / 270.0)
+	
+add = 0
+servo = serialServo.Servo(add)
 
 root = tk.Tk()
-label = tk.Label(root, text = "deg:0")
-label.pack()
+
+portLabel = tk.Label(root, text = "port")
+portLabel.pack()
+portBuff = tk.StringVar()
+portBuff.set("/dev/ttyAMA0")
+
+portEntry = tk.Entry(root, textvariable = portBuff)
+portEntry.pack()
+portEntry.bind("<Return>", setPort)
+
+buffer = tk.StringVar()
+buffer.set(str(add))
+
+posBuff = tk.StringVar()
+posBuff.set("0")
+
+
 scale1 = tk.Scale(root,
 				  label = "servo position",
 				  orient = "h",
-				  from_ = 3500, to = 11500,
+				  from_ = 0.0, to = 270.0, resolution = 0.1,
 				  command = scaleFunc)
 scale1.pack()
+
+posEntry = tk.Entry(root, textvariable = posBuff)
+posEntry.pack()
+posEntry.bind("<Return>", setPos)
+
+addLabel = tk.Label(root, text = "address:0")
+addLabel.pack()
+
+addEntry = tk.Entry(root, textvariable = buffer)
+addEntry.pack()
+addEntry.bind("<Return>", selectAdd)
+
 root.mainloop()
